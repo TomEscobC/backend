3853435026eabcc17a7514e25e245827ab49a95f
MariaFValdivia <mfvaldiviag@hotmail.com>
2025-02-12 20:53:12 -0300

    Arrego de login, ruta api/auth/login

--- src/models/usersModels.js
+++ src/models/usersModels.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
